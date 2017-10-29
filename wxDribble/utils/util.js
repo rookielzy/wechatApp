34842238bbada859897e5f3da20fc5d80ef80b9a
rookielzy <slimjetlzy@gmail.com>
2017-10-29 23:44:42 +0800

    Wechat App Dribble Init

---
+++ wxDribble/utils/util.js
@@ -0,0 +1,21 @@
+const formatTime = date => {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  const hour = date.getHours()
+  const minute = date.getMinutes()
+  const second = date.getSeconds()
+
+  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+}
+
+const formatNumber = n => {
+  n = n.toString()
+  return n[1] ? n : '0' + n
+}
+
+const clientAccessToken = '4e59f273273bebbf1db19d7fdc5c3a809e1becec7b79d943f9908c07289430c4';
+
+module.exports = {
+  formatTime: formatTime
+}
